49d40edf11cb1a403c742c086b171fd276d69664
Funnystyle <esero@hanmail.net>
2021-12-09 05:16:11 +0900

    css js

--- script3.js
+++ script3.js
@@ -1,5 +1,3 @@
-feather.replace();
-
 const controls = document.querySelector('.controls');
 const cameraOptions = document.querySelector('.video-options>select');
 const video = document.querySelector('video');
@@ -22,16 +20,21 @@ const constraints = {
       ideal: 1080,
       max: 1440
     },
+    facingMode: {
+      exact: 'environment'
+    }
   }
 };
 
-const getCameraSelection = async () => {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const videoDevices = devices.filter(device => device.kind === 'videoinput');
-  const options = videoDevices.map(videoDevice => {
-    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-  });
-  cameraOptions.innerHTML = options.join('');
+cameraOptions.onchange = () => {
+  const updatedConstraints = {
+    ...constraints,
+    deviceId: {
+      exact: cameraOptions.value
+    }
+  };
+
+  startStream(updatedConstraints);
 };
 
 play.onclick = () => {
@@ -52,17 +55,45 @@ play.onclick = () => {
   }
 };
 
+const pauseStream = () => {
+  video.pause();
+  play.classList.remove('d-none');
+  pause.classList.add('d-none');
+};
+
+const doScreenshot = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  screenshotImage.src = canvas.toDataURL('image/webp');
+  screenshotImage.classList.remove('d-none');
+};
+
+pause.onclick = pauseStream;
+screenshot.onclick = doScreenshot;
+
 const startStream = async (constraints) => {
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   handleStream(stream);
 };
+
 
 const handleStream = (stream) => {
   video.srcObject = stream;
   play.classList.add('d-none');
   pause.classList.remove('d-none');
   screenshot.classList.remove('d-none');
-  streamStarted = true;
+
+};
+
+
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
 };
 
 getCameraSelection();
